ba72f8478f58879a020af2823c98b420e5accabb
Victor Jambo <victormutaijambo@gmail.com>
2017-10-06 08:17:20 +0000

    user friendly_id

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
   has_many :videos
   has_many :comments
   mount_uploader :avatar, AvatarUploader
+  extend FriendlyId
+  friendly_id :username, use: :slugged
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171005124628) do
+ActiveRecord::Schema.define(version: 20171006073619) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -119,8 +119,10 @@ ActiveRecord::Schema.define(version: 20171005124628) do
     t.text     "bio"
     t.string   "avatar"
     t.string   "username"
+    t.string   "slug"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["slug"], name: "index_users_on_slug", unique: true
   end
 
   create_table "videos", force: :cascade do |t|
